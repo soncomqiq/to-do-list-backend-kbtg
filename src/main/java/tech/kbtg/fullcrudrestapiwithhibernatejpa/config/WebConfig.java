a390fb2e3704527237cf60540872b8d5c04a3c71
soncomqiq <nuttachai.ku@hotmail.com>
2023-07-05 22:52:28 +0700

    Update WebConfig.java

--- src/main/java/tech/kbtg/fullcrudrestapiwithhibernatejpa/config/WebConfig.java
+++ src/main/java/tech/kbtg/fullcrudrestapiwithhibernatejpa/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig
 			@Override
 			public void addCorsMappings( CorsRegistry registry )
 			{
-				registry.addMapping( "/**" ).allowedOrigins( "*" );
+				registry.addMapping( "/**" ).allowedOrigins( "*" ).allowedMethods("GET", "POST","PUT", "DELETE");
 			}
 		};
 	}
